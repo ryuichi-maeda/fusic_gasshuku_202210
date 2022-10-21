7aa0b247197d074d36cc24b9440531f14872f2d9
SunamotoMikiya <miki38gtr@gmail.com>
2022-10-21 15:32:35 +0900

    ログイン画面作成

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,31 @@
+import auth from "../firebase";
+import { useAuthContext } from "../context/AuthContext";
+
+const SignUp = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = event.target.elements;
+    auth.createUserWithEmailAndPassword(email.value, password.value);
+  };
+
+  return (
+    <div>
+      <h1>ユーザー登録</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input name="email" type="email" placeholder="email" />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button>登録</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
