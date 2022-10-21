7663aebe2a669c6545b5c3f5a944e18fe55d0f14
SunamotoMikiya <miki38gtr@gmail.com>
2022-10-21 17:04:34 +0900

    ログインページとログイン後のページの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <AuthProvider>
       <div style={{ margin: "2em" }}>
-        <SignUp />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </AuthProvider>
   );
