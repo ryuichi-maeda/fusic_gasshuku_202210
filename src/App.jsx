c78287b95a47c38e62146df178ebdb52e6a5c9ae
SunamotoMikiya <miki38gtr@gmail.com>
2022-10-21 15:32:54 +0900

    ログイン画面の表示処理追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import SignUp from "./components/SignUp";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <div style={{ margin: "2em" }}>
+        <SignUp />
+      </div>
+    </AuthProvider>
   );
 }
 
