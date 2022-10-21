84502fa33b928be633c5de6eeeee0d7e30c3cce1
ryuichi-maeda <uryu44213@gmail.com>
2022-10-21 14:09:24 +0900

    簡易ルーティング

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+    return (
+        <>
+            <Link to="/maeda" >maeda</Link>
+            <br />
+            <Link to="/sunamoto" >sunamoto</Link>
+            <br />
+            <Link to="/kawanishi" >kawanishi</Link>
+        </>
+    )
+}
